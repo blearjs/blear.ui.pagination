87ff7be2cf174b5369f26b239441ad0838e6170e
#云淡然 <cloudcome@163.com>
2017-04-14 10:58:27 +0800

    ~ examples

--- examples/index.js
+++ examples/index.js
@@ -7,6 +7,28 @@
 
 'use strict';
 
+var Pagination = require('../src/index');
 
+new Pagination({
+    el: '#simple',
+    mode: 'simple',
+    total: 20,
+    page: 3
+}).on('change', function (page, calback) {
+    calback({
+        page: page
+    });
+});
+
+new Pagination({
+    el: '#range',
+    mode: 'range',
+    total: 20,
+    page: 3
+}).on('change', function (page, calback) {
+    calback({
+        page: page
+    });
+});
 
 
